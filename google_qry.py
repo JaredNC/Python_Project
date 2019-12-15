f6dfaf26ae2726550abcbaed16b4b9125dcd5eec
Jared Crivello <jaredcrivello@gmail.com>
2019-12-15 19:16:29 +0100

    Improved google search to keep trying if results are empty

--- google_qry.py
+++ google_qry.py
@@ -11,42 +11,47 @@ from bs4 import BeautifulSoup
 # print(chatbot_query('how old is samuel l jackson'))
 
 
-def chatbot_query(query, index=0):
+def chatbot_query(query, index=0, tries=5):
     fallback = 'Sorry, I cannot think of a reply for that.'
     result = ''
 
     try:
-        search_result_list = list(search(query+" -site:youtube.com", tld="co.in", num=10, stop=3, pause=1))
+        search_result_list = list(search(query+" -site:youtube.com", tld="co.in", num=10, stop=tries, pause=1))
 
         # page1 = requests.get(search_result_list[index])
         # page2 = requests.get(search_result_list[index+1])
         # page3 = requests.get(search_result_list[index+2])
         # page = page1 + page2 + page3
 
-        page = requests.get(search_result_list[index])
+        counter = index
+        while (counter < tries) & (result == ''):
+            page = requests.get(search_result_list[counter])
 
-        tree = html.fromstring(page.content)
+            # tree = html.fromstring(page.content)
 
-        soup = BeautifulSoup(page.content, features="lxml")
+            soup = BeautifulSoup(page.content, features="lxml")
+            # print(soup)
 
-        article_text = ''
-        article = soup.findAll('p')
-        for element in article:
-            article_text += '\n' + ''.join(element.findAll(text = True))
-        article_text = article_text.replace('\n', '')
-        first_sentence = article_text.split('.')
-        first_sentence = first_sentence[0] + '.'
-        # first_sentence = first_sentence[0] + " " + first_sentence[1] + " " + first_sentence[2]
-        # first_sentence = article_text[:500] + '..'
+            article_text = ''
+            article = soup.findAll('p')
+            for element in article:
+                article_text += '\n' + ''.join(element.findAll(text = True))
+            article_text = article_text.replace('\n', '')
+            first_sentence = article_text.split('.')
+            first_sentence = first_sentence[0]
+            # first_sentence = first_sentence[0] + " " + first_sentence[1] + " " + first_sentence[2]
+            # first_sentence = article_text[:500] + '..'
 
-        chars_without_whitespace = first_sentence.translate(
-            { ord(c): None for c in string.whitespace }
-        )
+            chars_without_whitespace = first_sentence.translate(
+                { ord(c): None for c in string.whitespace }
+            )
 
-        if len(chars_without_whitespace) > 0:
-            result = first_sentence
-        else:
-            result = fallback
+            if len(chars_without_whitespace) > 0:
+                result = first_sentence + '.'
+            else:
+                result = ''
+                print('Failed '+str(counter))
+                counter += 1
 
         return result
     except:
