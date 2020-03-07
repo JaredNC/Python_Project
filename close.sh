3fbef69bfc3fc383d2ed5650c5a95a1599c48c2a
Jared Crivello <jaredcrivello@gmail.com>
2020-03-07 17:07:09 +0100

    Attempt to have api load on start.sh and close on close.sh

--- close.sh
+++ close.sh
@@ -1,2 +1,4 @@
 kill -9 `cat save_pid.txt`
 rm save_pid.txt
+kill -9 `cat save_pid_2.txt`
+rm save_pid_2.txt
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 git pull origin master
 nohup python telegram_bot_test.py > my.log 2>&1 &
 echo $! > save_pid.txt
+nohup gunicorn -- bind=0:9999 api:app > my.log 2>&1 &
+echo $! > save_pid_2.txt
