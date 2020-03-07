b6af639d476ff74ddf5a7fb3ab8dcaab2a89379e
Jared Crivello <jaredcrivello@gmail.com>
2020-03-07 16:50:56 +0100

    api with flask to accept incoming http posts from forum server

---
+++ api.py
@@ -0,0 +1,45 @@
+import flask
+from flask import request, jsonify
+import battle as bat
+import newciv_bot as nc
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1>" \
+           "<p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+@app.route('/api', methods=['GET'])
+def api_id():
+    try:
+        if 'id1' in request.args:
+            team1 = min(int(request.args['id1']), 5000)
+        else:
+            return "Team 1 invalid."
+
+        if 'id2' in request.args:
+            team2 = min(int(request.args['id2']), 5000)
+        else:
+            return "Team 2 invalid."
+
+        team1 = 0 if team1 == team2 else team1
+    except:
+        return "Problem with team ids."
+
+    try:
+        new_b = bat.BattleBB(team1, team2)
+        test, winner = new_b.battle_bb()
+        new = nc.NewcivLogin()
+        new_p = new.make_newpost(test, 1054931)
+        print(f"Success! Winner: {winner}")
+        return "Success."
+    except:
+        print(f"Failure! Team1: {team1} Team2: {team2}")
+        return "Failure."
+
+
+app.run()
